68ce140f4846909392453a70b38360b9c7510a40
sksazid01 <1.sksazid@gmail.com>
2026-04-22 11:56:02 +0600

    feat: wire HomeComponent data to HousingLocationComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
-import {HomeComponent} from './home/home.component'
-import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
-    HousingLocationComponent,
   ],
   template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-    <app-housing-location></app-housing-location>
-  </section>
+    <app-home></app-home>
   `,
   styleUrls: ['./app.component.css'],
 })
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing" *ngIf="housingLocation as h">
+      <img class="listing-photo" [src]="h.photo" [alt]="h.name">
+      <h2 class="listing-heading">{{ h.name }}</h2>
+      <p class="listing-location">{{ h.city }}, {{ h.state }}</p>
+      <p>Available Units: {{ h.availableUnits }}</p>
+      <p>Wifi: {{ h.wifi ? 'Yes' : 'No' }}</p>
+      <p>Laundry: {{ h.laundry ? 'Yes' : 'No' }}</p>
+    </section>
   `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
