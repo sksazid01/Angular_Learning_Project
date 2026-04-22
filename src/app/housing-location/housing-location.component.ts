24ea960488c2f4b928a131fff1c74ee70fc972d4
sksazid01 <1.sksazid@gmail.com>
2026-04-22 12:58:01 +0600

    Use *ngFor in template

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,14 +7,11 @@ import { HousingLocation } from '../housinglocation';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <section class="listing" *ngIf="housingLocation as h">
-      <img class="listing-photo" [src]="h.photo" [alt]="h.name">
-      <h2 class="listing-heading">{{ h.name }}</h2>
-      <p class="listing-location">{{ h.city }}, {{ h.state }}</p>
-      <p>Available Units: {{ h.availableUnits }}</p>
-      <p>Wifi: {{ h.wifi ? 'Yes' : 'No' }}</p>
-      <p>Laundry: {{ h.laundry ? 'Yes' : 'No' }}</p>
-    </section>
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
   `,
   styleUrl: './housing-location.component.css'
 })
