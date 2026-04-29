86215546998e0058041255c6503f6877a6314d35
sksazid01 <1.sksazid@gmail.com>
2026-04-29 16:44:12 +0600

    Validators and custom validators

---
+++ form-validation-angular-17/src/app/shared/forbidden-name.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, Input } from '@angular/core';
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+} from '@angular/forms';
+
+/** A hero's name can't match the given regular expression */
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => { // takes an Angular control object 
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? { forbiddenName: { value: control.value } } : null;
+  };
+}
+
+@Directive({
+  selector: '[appForbiddenName]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: ForbiddenValidatorDirective,
+      multi: true,
+    },
+  ],
+  standalone: true,
+})
+export class ForbiddenValidatorDirective implements Validator {
+  @Input('appForbiddenName') forbiddenName = '';
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    return this.forbiddenName
+      ? forbiddenNameValidator(new RegExp(this.forbiddenName, 'i'))(control)
+      : null;
+  }
+}
